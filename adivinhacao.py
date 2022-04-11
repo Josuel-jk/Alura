e9a79e5595db937b764e67880e970509859899e0
Supertramp <josuel.mma@gmail.com>
2022-04-11 15:26:00 -0300

    Criado jogo adivinhação

---
+++ adivinhacao.py
@@ -0,0 +1,54 @@
+import random
+
+def jogar():
+
+    print("*********************************")
+    print("Bem vindo ao jogo de Adivinhação!")
+    print("*********************************")
+
+    numero_secreto = random.randint(0,50)
+    total_de_tentativas = 0
+    pontos = 1000
+
+
+    print("Qual o nivel de dificuldade?")
+    print("(1) Fácil (2) Médio (3) Dificil ")
+
+    nível = int(input("Defina a dificuldade: "))
+
+    if(nível == 1):
+        total_de_tentativas = 20
+    elif(nível == 2):
+        total_de_tentativas = 10
+    else:
+        total_de_tentativas = 5
+
+    for rodada in range(1, total_de_tentativas + 1):
+        print("Tentativa {} de {}".format(rodada, total_de_tentativas))
+        chute_str = input("Digite um número entre 1 e 100: ")
+        print("Você digitou " , chute_str)
+        chute = int(chute_str)
+
+        if(chute < 1 or chute > 100):
+            print("Você deve digitar um número entre 1 e 100!")
+            continue
+
+        acertou = chute == numero_secreto
+        maior   = chute > numero_secreto
+        menor   = chute < numero_secreto
+
+        if(acertou):
+            print("Você acertou e fez {} pontos!".format(pontos))
+            break
+        else:
+            if(maior):
+                print("Você errou! O seu chute foi maior do que o número secreto.")
+            elif(menor):
+                print("Você errou! O seu chute foi menor do que o número secreto.")
+            pontos_perdidos = abs(numero_secreto - chute)
+            pontos = pontos - pontos_perdidos
+
+    print("Fim do jogo")
+
+if (__name__ == "__main__"):
+    jogar()
