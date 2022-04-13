358c2438b707cf26dfa43440462fbd32ee77f602
josuel-jk <josuel.mma@gmail.com>
2022-04-13 16:03:10 -0300

    finalizando jogo da forca

--- forca.py
+++ forca.py
@@ -1,27 +1,52 @@
+import random
+
 def jogar():
     print("*********************************")
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
 
-    palavra_secreta = "banana"
+    arquivo = open("palavras.txt", "r")
+    palavras = []
+
+    for linha in arquivo:
+        linha = linha.strip()
+        palavras.append(linha)
+
+    arquivo.close()
+
+    numero = random.randrange(0,len(palavras))
+    palavra_secreta = palavras[numero].upper()
+
+    letras_acertadas = ["_" for letra in palavra_secreta]
 
     enforcou = False
     acertou = False
+    erros = 0
+
+    print(letras_acertadas)
 
     while(not enforcou and not acertou):
 
-        chute =input("Qual a letra? ")
+        chute = input("Qual a letra? ")
+        chute = chute.strip().upper()
 
-        for letra in palavra_secreta:
-            if(chute == letra):
-                print(chute)
+        if(chute in palavra_secreta):
+            index = 0
+            for letra in palavra_secreta:
+                if (chute == letra):
+                    letras_acertadas[index] = letra
+                index += 1
+        else:
+            erros += 1
 
+        enforcou = erros == 6
+        acertou = "_" not in letras_acertadas
+        print(letras_acertadas)
 
-        print("jogando")
-
-
-
-
+    if(acertou):
+        print("Você ganhou!!")
+    else:
+        print("Você perdeu!!")
     print("Fim do jogo")
 
 if (__name__ == "__main__"):
