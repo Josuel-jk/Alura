626653acaa1105ef3a4519791504dd6dcfe2ee6b
josuel-jk <josuel.mma@gmail.com>
2022-04-13 10:17:10 -0300

    criando jogo forca

--- forca.py
+++ forca.py
@@ -1,9 +1,24 @@
-
-
 def jogar():
     print("*********************************")
     print("Bem vindo ao jogo de Forca!")
     print("*********************************")
+
+    palavra_secreta = "banana"
+
+    enforcou = False
+    acertou = False
+
+    while(not enforcou and not acertou):
+
+        chute =input("Qual a letra? ")
+
+        for letra in palavra_secreta:
+            if(chute == letra):
+                print(chute)
+
+
+        print("jogando")
+
 
 
 
