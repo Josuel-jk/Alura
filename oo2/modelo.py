dc2102448ea16c7eea14c3b768564f37109d03fb
josuel-jk <josuel.mma@gmail.com>
2022-04-19 15:51:52 -0300

    aprendendo sub classe, superclasse, polimorfismo, método dunder

--- oo2/modelo.py
+++ oo2/modelo.py
@@ -21,10 +21,16 @@ class Programa:
         def nome(self, novo_nome):
             self._nome = novo_nome.title()
 
+        def __str__(self):
+            return f'{self._nome} - {self.ano} - {self._likes} Likes'
+
 class Filme(Programa):
     def __init__(self, nome, ano, duracao):
         super().__init__(nome,ano)
         self.duracao = duracao
+
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.duracao} min - {self._likes} Likes'
 
 
 class Serie(Programa):
@@ -32,9 +38,41 @@ class Serie(Programa):
         super().__init__(nome,ano)
         self.temporadas = temporadas
 
+    def __str__(self):
+        return f'{self._nome} - {self.ano} - {self.temporadas} Temporadas - {self._likes} Likes'
+
+
+class Playlist:
+    def __init__(self, nome, programas):
+        self.nome = nome
+        self._programas = programas
+
+    def __getitem__(self, item):
+        return self._programas[item]
+
+    @property
+    def listagem(self):
+        return self._programas
+
+
+    def __len__(self):
+        return len(self._programas)
+
+
+
+
 
 vingadores = Filme('vingadores - guerra infinita', 2018, 160)
-print(f'{vingadores._nome} - {vingadores.duracao} : {vingadores._likes}')
-
+tmp = Filme('todo mundo em panico', 1999, 100)
 atlanta = Serie('atlanta', 2018, 2)
-print(f'{atlanta._nome} - {atlanta.duracao} : {atlanta._likes}')
+demolidor = Serie("demolidor", 2016, 2)
+
+
+
+filmes_e_series = [vingadores, atlanta, demolidor, tmp]
+playlist_fim_de_semana = Playlist('fim de semana', filmes_e_series)
+
+print(f'Tamanho da Playlist: {len(playlist_fim_de_semana)}')
+
+for programa in playlist_fim_de_semana:
+    print(programa)
