0dc32c16e513ecf13a8898faf3c9578a18bf849d
josuel-jk <josuel.mma@gmail.com>
2022-04-20 16:43:21 -0300

    refatorações  com o método regex em strings

--- Extrator-url/extrator_CEP.py
+++ Extrator-url/extrator_CEP.py
@@ -4,7 +4,7 @@ endereco = "Rua da Flores 72, apartamento 1002, Laranjeiras, Rio de Janeiro, RJ,
 
 # 5 dígitos + hífen(opcional) + 3 digitos
 
-padrao = re.compile('[0123456789][0123456789][0123456789][0123456789][0123456789][-]?[0123456789][0123456789][0123456789]')
+padrao = re.compile('[0-9]{5}[-]{0,1}[0-9]{3}')
 busca = padrao.search(endereco) # Match
 if busca:
     cep = busca.group()
